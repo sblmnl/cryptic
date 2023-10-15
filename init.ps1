2d72e6bec58cfb9d3d6d6e141226826d170da1e7
sblmnl <sblmnl@users.noreply.github.com>
2023-10-15 11:40:46 -0400

    add ssl/tls support

--- init.ps1
+++ init.ps1
@@ -1,6 +1,15 @@
+# generate the ssl/tls dev cert
+dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\cryptic.pfx -p password
+dotnet dev-certs https --trust
+
 # populate the .env file
 Add-Content -Path ".env" -Value @"
+# database
 DB_NAME=cryptic
 DB_USER=postgres
 DB_PASSWORD=password
+
+# ssl/tls
+CERT_PASSWORD=password
+CERT_PATH=/https/cryptic.pfx
 "@
--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# generate the ssl/tls dev cert
+dotnet dev-certs https -ep ${HOME}/.aspnet/https/cryptic.pfx -p password
+dotnet dev-certs https --trust
+
 # populate the .env file
 cat <<EOF > .env
+# database
 DB_NAME=cryptic
 DB_USER=postgres
 DB_PASSWORD=password
+
+# ssl/tls
+CERT_PASSWORD=password
+CERT_PATH=/https/cryptic.pfx
 EOF
