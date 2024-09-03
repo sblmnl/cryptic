5c7fdcdc94c1690ee672a727448e11c6a25882a4
sblmnl <sblmnl@users.noreply.github.com>
2024-09-02 23:12:21 -0400

    dedupe

--- src/WebAPI/Features/Notes/MetaLinks.cs
+++ src/WebAPI/Features/Notes/MetaLinks.cs
@@ -4,13 +4,15 @@ namespace Cryptic.WebAPI.Features.Notes;
 
 public static class MetaLinks
 {
+    private static string GetNoteBaseUri(HttpRequest req, Domain.Note note) => req.GetBaseUri() + $"/{note.Id}";
+    
     public static MetaLink GetReadNoteLink(this Domain.Note note, HttpRequest req)
     {
-        return new(req.GetBaseUri() + $"/{note.Id}", "read-note", HttpMethods.Get);
+        return new(GetNoteBaseUri(req, note), "read-note", HttpMethods.Get);
     }
     
     public static MetaLink GetDestroyNoteLink(this Domain.Note note, HttpRequest req)
     {
-        return new(req.GetBaseUri() + $"/{note.Id}", "destroy-note", HttpMethods.Delete);
+        return new(GetNoteBaseUri(req, note), "destroy-note", HttpMethods.Delete);
     }
 }
