0fa9c03403e33603bf7d5d676a232cddc706c8c2
sblmnl <sblmnl@users.noreply.github.com>
2024-09-16 17:23:58 -0400

    add pre-commit hook for formatting and linting client code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./main.css";
 
@@ -11,7 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>Hello, world!</h1>,
-  }
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
